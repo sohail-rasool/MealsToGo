88336b2a0ba1adf2ace2051976dd1fb27c03e2a6
Sohail <sohailrasoolabbasi21@gmail.com>
2023-11-13 12:36:15 +0500

    Added rating component

--- src/features/restaurants/components/RestaurantsInfoCard.js
+++ src/features/restaurants/components/RestaurantsInfoCard.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from 'react-native-svg';
+import { startSVG } from "../../../../assets/star";
 
 const Title = styled.Text`
-  color: ${(props)=>props.theme.colors.ui.primary};
-  font-family: ${(props)=>props.theme.fonts.heading};
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.title};
+  margin-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const AddressInfo = styled.Text`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  margin-bottom: ${(props) => props.theme.space[1]};
 `;
 
 const RestaurantCard = styled(Card)`
-  padding: ${(props)=>props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const CoverImg = styled(Card.Cover)`
-  margin-bottom: ${(props)=>props.theme.space[3]};
+  margin-bottom: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantsInfoCard = ({ restaurant = {} }) => {
@@ -27,10 +42,17 @@ const RestaurantsInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+  const ratingArr = Array.from(new Array(Math.floor(rating)))
   return (
     <RestaurantCard>
       <CoverImg source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Card.Content>
+        <Title>{name}</Title>
+        <Rating>
+          {ratingArr?.map(()=> <SvgXml xml={startSVG} width="20px" height="20px" /> )}
+        </Rating>
+        <AddressInfo>{address}</AddressInfo>
+      </Card.Content>
     </RestaurantCard>
   );
 };
